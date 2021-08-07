1dd369dfa051ca4ea4dbc69e8a2811d0198ce87d
Getaante Yilma <GY@Getaantes-MacBook-Air.local>
2021-08-07 19:08:19 -0400

    upated as of 8/7

---
+++ Feedback/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug=True
+    # app.config['SQL_DATABASW_URI] = posgress----- Issue.   
+    app.config['SQL_DATABASE_URI']= ''
+else:
+    app.debug=False 
+    app.config['SQL_DATABASE_URI']= ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False 
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__='feedback'
+    id = db.Column(db.Integer,primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self,cusomter,dealer,rating,comments):
+        self.customer=cusomter
+        self.dealer=dealer
+        self.rating=rating
+        self.comments=comments
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        return render_template('success.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
+
